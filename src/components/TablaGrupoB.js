e81277774161c36ec11b8671f1e56c45873f4fa5
FabianMongui <68082997+FabianMongui@users.noreply.github.com>
2024-03-03 00:04:23 -0500

    Add files via upload

---
+++ src/components/TablaGrupoB.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import equiposData from '../data/grupoBData.json'
+
+function ordenarDatos() {
+  equiposData.sort((a, b) => b.puntos - a.puntos);
+}
+
+function TablaPosiciones() {
+  //const equipos = generateRandomData();
+  ordenarDatos();
+  console.log("🚀 ~ TablaPosiciones ~ equipos:", equiposData)
+
+  return (
+    <div className="table-responsive mt-5">
+      <h2>Grupo B</h2>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th className="col-4">Equipo</th>
+            <th style={{ width: '15%' }}>Puntos</th>
+            <th style={{ width: '15%' }}>Partidos Jugados</th>
+            <th style={{ width: '15%' }}>Partidos Ganados</th>
+            <th style={{ width: '15%' }}>Partidos Perdidos</th>
+            <th style={{ width: '15%' }}>Goles</th>
+          </tr>
+        </thead>
+        <tbody>
+          {equiposData.map((equiposData, index) => (
+            <tr key={index}>
+              <td className="col-3" style={{ fontWeight: '500' }}>{equiposData.equipo}</td>
+              <td style={{ width: '15%' }}>{equiposData.puntos}</td>
+              <td style={{ width: '15%' }}>{equiposData.partidosJugados}</td>
+              <td style={{ width: '15%' }}>{equiposData.partidosGanados}</td>
+              <td style={{ width: '15%' }}>{equiposData.partidosPerdidos}</td>
+              <td style={{ width: '15%' }}>{equiposData.goles}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TablaPosiciones;
